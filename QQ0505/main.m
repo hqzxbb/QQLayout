dedcb2df2c1ed1cedc107b7697a7139338f41c98
黄宾宾 <hqzxbinbin@qq.com>
2016-03-14 17:07:50 +0800

    第一次提交
    第一次提交

---
+++ QQ0505/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQ0505
+//
+//  Created by 黄宾宾 on 5/5/15.
+//  Copyright (c) 2015 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
